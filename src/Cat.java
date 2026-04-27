2a8aed3e4e33d54120e4097e8df80db11685e7f6
Luis J Guardia <lguardias@iesch.org>
2026-04-27 19:13:34 +0200

    Función toString más completa

--- src/Cat.java
+++ src/Cat.java
@@ -2,7 +2,5 @@ public class Cat extends Animal implements Pet {
     public String playful;
     public String breed;
 
-    public String toString(){
-        return playful;
-    }
+    public String toString(){ return "Raza: " + breed + "; " + playful; }
 }
--- src/Fish.java
+++ src/Fish.java
@@ -1,8 +1,8 @@
-public class Fish extends Animal implements Pet{
+public class Fish extends Animal implements Pet {
     public String playful;
     public int swimspeed;
 
     public String toString(){
-        return playful;
+        return "Velocidad: " + swimspeed + "; " + playful;
     }
 }
